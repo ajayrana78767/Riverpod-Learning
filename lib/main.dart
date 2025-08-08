936a7547377a51b819ccb5036c775c04d4477c8d
ajayrana78767 <148841039+ajayrana78767@users.noreply.github.com>
2025-08-08 10:56:58 +0530

    Add: Implemented StateNotifierProvider with basic counter example

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_learning/State%20Notifier%20Provider/counter_demo.dart';
 
-final counterProvider = StateProvider<int>((ref) => 0);
+final counterProvider = StateNotifierProvider<CounterDemo, int>(
+  (ref) => CounterDemo(),
+);
 void main() {
   runApp(ProviderScope(child: const MyApp()));
 }
@@ -15,110 +18,27 @@ class MyApp extends StatelessWidget {
   }
 }
 
-/// First way why a extends ConsumerWidget
-
-// class MyHomePage extends ConsumerWidget {
-//   const MyHomePage({super.key});
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final name = ref.watch(nameProvider);
-//     return Scaffold(body: Center(child: Text(name)));
-//   }
-// }
-
-///  Get the value when the stateless widget
-
-// class MyHomePage extends StatelessWidget {
-//   const MyHomePage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: Consumer(
-//           builder: (context, ref, child) {
-//             final name = ref.watch(nameProvider);
-//             return Text(name);
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-/// How to read a value from when a STF widget
-// class MyHomePage extends ConsumerStatefulWidget {
-//   const MyHomePage({super.key});
-
-//   @override
-//   ConsumerState<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends ConsumerState<MyHomePage> {
-//   @override
-//   initState() {
-//     super.initState();
-//     final name = ref.read(nameProvider);
-//     debugPrint(name);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final name = ref.watch(nameProvider);
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.blue,
-//         centerTitle: true,
-//         title: Text('Riverpod', style: TextStyle(color: Colors.white)),
-//       ),
-//       body: Center(child: Text(name)),
-//     );
-//   }
-// }
-
-///* State Provider
 class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final count = ref.watch(counterProvider);
-    ref.listen(counterProvider, (previous, next) {
-      if (next == 10) {
-        ScaffoldMessenger.of(
-          context,
-        ).showSnackBar(SnackBar(content: Text('The value is $next')));
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('The count is updated now is $next')),
-        );
-      }
-    });
-    debugPrint('Full screen is builded.....');
+    debugPrint('Full Screen is builded........');
+    final counter = ref.watch(counterProvider);
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.teal.shade100,
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text('State Provider', style: TextStyle(color: Colors.white)),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.refresh, color: Colors.white),
-            onPressed: () {
-              //   ref.invalidate(counterProvider);
-              ref.invalidate(counterProvider);
-            },
-          ),
-        ],
+        backgroundColor: Colors.teal,
+        elevation: 24,
+        title: Text(
+          'State Notifier Provider',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
       ),
-      body: Center(
-        child: Text(count.toString(), style: TextStyle(fontSize: 30)),
-      ),
+      body: Center(child: Text(counter.toString(),style: TextStyle(fontSize: 36,fontWeight: FontWeight.bold),)),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.teal,
         onPressed: () {
-          //   ref.read(counterProvider.notifier).state++;
-          ref.read(counterProvider.notifier).update((state) => state + 1);
+          ref.read(counterProvider.notifier).increment();
         },
         child: Icon(Icons.add, color: Colors.white),
       ),
